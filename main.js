61e7a6b68e3098201e28ff17409354450e0de486
yoo-donggon <96458123+yoo-donggon@users.noreply.github.com>
2022-03-20 22:45:29 +0900

    최종

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//make navbar transparent
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
@@ -13,17 +14,23 @@ document.addEventListener('scroll', () =>{
     }
 });
 
-
-//Handle scrolling
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link==null) {
-        return;
-    }
-    scrollIntoView(link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
+
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 
@@ -65,3 +72,7 @@ function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView ({behavior: "smooth", block: "center"});
 }
+
+
+
+
